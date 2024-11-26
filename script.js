89567332f536d9b91b5a9ce62812c40442443580
Ahimelec Chia <ahimelecc123@gmail.com>
2024-11-26 15:49:00 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Placeholder for interactivity. Add any desired functionality here.
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Tech Coffee: Página cargada con éxito");
+});
